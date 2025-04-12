d22c8be3b845b0a77aa0536ce8c9a31eb707f2ac
kaydee <hudafaizaah@gmail.com>
2025-04-13 08:51:33 +0900

    final before tests

--- app.py
+++ app.py
@@ -2,18 +2,12 @@ from flask import Flask, render_template, request, jsonify
 from time import time
 from aiChatbot import PerplexityAIChatbot 
 from news import NewsFetcher
+from flask import session, redirect, url_for
 
 app = Flask(__name__)
 
 model = PerplexityAIChatbot()
 news_fetcher = NewsFetcher()
-
-DISASTER_API_KEY = 'your_disaster_api_key'
-DISASTER_API_URL = 'https://api.safetydata.go.kr/openapi/service/rest/DisasterMsg2/getDisasterMsgList'
-
-#PERPLEXITY_API_KEY=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
-#UPSTAGE_API_KEY=up_LQPEk4u7eRVceg4D3D6YfrUxU4X6W
-#NEWSAPI_KEY=a1949a55a1194792bce313981c4aaaaa
 
 RATE_LIMIT = 5 # Number of requests allowed per user in the time window
 WINDOW = 60
